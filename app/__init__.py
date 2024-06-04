88db5ca8d552493be782152864896a63af97605f
Alexdrcastillo <alexdrcastillo9@gmail.com>
2024-06-04 01:17:22 -0400

    sg

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
 # Configuración de CORS
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://doqia-frontend.com"]}})
+CORS(app, resources={r"/*": {"origins": "https://doqia-client.onrender.com"}})
 
 # Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://database_doqia_user:9iCarEpLArP1VsUXdAep3iDFK9MUx9J3@dpg-cp78vomd3nmc73bnp5u0-a.oregon-postgres.render.com/database_doqia'
